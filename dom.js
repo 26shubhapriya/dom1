8c1089860aa80a303ebce30de534d582f852b9cc
26shubhapriya <shubhapriya1998@gmail.com>
2023-04-05 19:55:28 +0530

    creating node and modifying node

--- dom.js
+++ dom.js
@@ -82,12 +82,12 @@
 // var lastItem = document.querySelector('.list-group-item:last-child');
 // lastItem.style.color = 'blue';
 
- var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-// secondItem.style.color = 'coral';
-secondItem.style.backgroundColor = 'green';
+//  var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// // secondItem.style.color = 'coral';
+// secondItem.style.backgroundColor = 'green';
 
-var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.visibility = 'hidden'
+// var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.visibility = 'hidden'
 
 //queryselectorall //
 
@@ -106,3 +106,82 @@ thirdItem.style.visibility = 'hidden'
 // //     even[i].style.backgroundColor = '#ccc';
 //  }
 
+//traversing the dom //
+
+//var itemList = document.querySelector('#items');
+//parentNode 
+//console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+//parentElement 
+ //console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+//childNodes
+// console.log(itemList.childNodes);
+
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow';
+
+//firstchild
+// console.log(itemList.firstChild);
+
+//firstelementchild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello 1'
+
+//lastchild
+// console.log(itemList.lastChild);
+
+//lastelementchild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Hello 4';
+
+//nextSibling
+//console.log(itemList.nextSibling);
+//nextelementsibling
+//console.log(itemList.nextElementSibling);
+
+//previoussibling
+//console.log(itemList.previousSibling);
+//previouselementsibling //
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color='green'
+
+//createElement
+
+//create a div
+//var newDiv = document.createElement('div');
+
+//add class
+//newDiv.className = 'hello';
+
+//add id
+//newDiv.id = 'hello1';
+
+//add attribute
+//newDiv.setAttribute('title','Hello Div');
+
+//create text node
+//var newDivText = document.createTextNode('Hello World');
+
+//add text to div
+//newDiv.appendChild(newDivText);
+
+//var container = document.querySelector('header .container');
+//var h1 = document.querySelector('header h1');
+ 
+//console.log(newDiv);
+
+//newDiv.style.fontSize = '10px'
+
+//container.insertBefore(newDiv,h1);
+
+//add hello world before item 1
+
+// var container2 = document.querySelector('#items');
+// var listgroup = document.querySelector('.list-group-item');
+// container2.insertBefore(newDiv,listgroup);
